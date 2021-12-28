826a81f281fab34f292f8c4555c31f42adcc0e15
Babasaheb Argade <babasahebargade09@gmail.com>
2021-12-28 23:29:52 +0530

    first Window Program

---
+++ Fixed Function Pipeline/Windows/01_Windows/WindowFirst.cpp
@@ -0,0 +1,88 @@
+// Header File
+#include<Windows.h>
+
+// Gloabal function declaration
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+// Entry Point function
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmsLine, int iCmdShow)
+{
+	//variable declaration
+	WNDCLASSEX wndclass;
+	HWND hwnd;
+	MSG msg;
+	TCHAR szAppName[] = TEXT("My Window");
+
+	//code
+	//initilization of wndclass structure
+	
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.hInstance = hInstance;
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.lpszClassName = szAppName;
+	wndclass.lpszMenuName = NULL;
+	wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+	// Register WNDCLASSEX
+
+	RegisterClassEx(&wndclass);
+
+	// create the window
+	
+	hwnd = CreateWindow(szAppName,
+		TEXT("Babasaheb Haribhau Argade"),
+	    WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	// showwindow
+
+	ShowWindow(hwnd, iCmdShow);
+
+	// update the window
+
+	UpdateWindow(hwnd);
+
+	//message loop
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return ((int)msg.wParam);
+
+}
+
+// callback function defination
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wparam, LPARAM lParam)
+{
+	//code
+
+	switch (iMsg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+
+	default:
+		break;
+	}
+
+	return (DefWindowProc(hwnd, iMsg, wparam, lParam));
+}
